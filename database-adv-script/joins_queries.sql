700d30892fd55178db9df5fd6d6eafd1eef7c1f1
obum <obumanichebe@gmail.com>
2025-07-01 11:27:20 +0100

    Write Complex Queries with Joins

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,12 @@
+SELECT a.user_id, a.first_name, b.booking_id 
+FROM users a
+INNER JOIN bookings b ON a.user_id = b.user_id;
+
+SELECT * 
+FROM properties a
+LEFT JOIN reviews b ON a.property_id = b.property_id;
+
+SELECT * 
+FROM users u
+FULL OUTER JOIN bookings b ON u.user_id = b.user_id;
+
